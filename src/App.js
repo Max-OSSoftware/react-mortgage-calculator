263f8699868a9b6867e3b09f9d25512e480558ed
zapboy216 <zapboy216@netzero.com>
2023-12-28 09:47:27 -0500

    Add responsive layout and components to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
-import './App.css';
+import React, { useState } from "react";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
+import Navbar from "./Components/Navbar";
+import Result from "./Components/Result";
+import SliderSelect from "./Components/SliderSelect";
+import TenureSelect from "./Components/TenureSelect";
+
 
 function App() {
   return (
     <div className="App">
-   <Navbar />
-    </div>
+  <Navbar />
+  <Container maxWidth="xl" sx={{marginTop:4}}>
+    <Grid container spacing={5} alignItems="center">
+      <Grid item xs={12} md={6}>
+        <SliderSelect />
+        <TenureSelect />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Result/>
+      </Grid>
+    </Grid>
+  </Container>
+</div>
+
   );
 }
 
