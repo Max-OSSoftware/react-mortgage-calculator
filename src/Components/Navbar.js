081d48270199e9cbf0c8a75e5370178faf183c7b
zapboy216 <zapboy216@netzero.com>
2023-12-31 13:55:44 -0500

    Remove unnecessary link in Navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,6 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Container } from "@mui/system";
-import Link from "@mui/material/Link";
 
 const Navbar = () => {
   return (
@@ -10,12 +9,9 @@ const Navbar = () => {
       <Container maxWidth="xl">
         <Toolbar>
           <Typography variant="h5">Mortgage Calculator</Typography>
+          
         </Toolbar>
-        <Typography variant="a">
-          <Link href="https://twitter.com/zapboy216" target="_blank" rel="noopener" text-color="white">
-            @zapboy216
-          </Link>
-        </Typography>
+        <Typography variant="p"> zapboy216</Typography>
       </Container>
     </AppBar>
   );
